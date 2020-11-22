60bee119925729d86d8bb04e39fa4a0b2c0e0e26
Karoline Alencar <karolinedalencar@gmail.com>
2020-11-22 15:49:41 -0300

    add mongodb base

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 require('dotenv').config()
+const amqp = require('amqplib')
+const { MongoClient } = require('mongodb')
 
-const amqp = require('amqplib');
-const { RABBITMQ_URL } = process.env
+const { RABBITMQ_URL, MONGODB_URL } = process.env
 
-const consumeQueue = async () => {
-    const connection = await amqp.connect(RABBITMQ_URL)
-    const channel = await connection.createChannel()
-    const queue = 'names';
-
-    await connection.createChannel();
-    await channel.assertQueue(queue, { durable: true });
-
-    await channel.consume(queue, msg => {
-        const name =  msg.content.toString()
-        channel.ack(msg);
-    });
+const connectToMongoDB = async () => {
+    const client = await MongoClient.connect(MONGODB_URL, { useUnifiedTopology: true })
+    return client
 }
 
-consumeQueue()
+const consumeQueue = async mongoDatabase => {
+    const connection = await amqp.connect(RABBITMQ_URL)
+    const channel = await connection.createChannel()
+    const queue = 'names'
+
+    await connection.createChannel()
+    await channel.assertQueue(queue, { durable: true })
+
+    await channel.consume(queue, async msg => {
+        const name =  msg.content.toString()
+        await saveNameOnDatabase(mongoDatabase, name)
+        channel.ack(msg)
+    })
+}
+
+const saveNameOnDatabase = async (mongoDatabase, name) => {
+    const collection = mongoDatabase.collection('name')
+    await collection.insertOne({ name })
+}
+
+const main = async () => {
+    const mongoClient = await connectToMongoDB()
+    const mongoDatabase = mongoClient.db('names');
+    await consumeQueue(mongoDatabase)
+}
+
+main()
